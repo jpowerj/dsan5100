24dd68c50e6d3e079682fb8a5066f30eef59895c
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-09-21 20:45:11 -0400

    Initial commit

---
+++ _post_pw_protect.py
@@ -0,0 +1,6 @@
+import shutil
+
+src_fpath = "./_index-template.html"
+target_fpath = "./docs/index.html"
+shutil.copyfile(src_fpath, target_fpath)
+print(f"Password-protection index copied to {target_fpath}")
